e384ede6ff310e5c2b4eea118d3aa891dcdb911a
wdev-sai <saiteja.bvn@gmail.com>
2024-10-13 22:48:24 +0530

    SalonSense

--- Haircut_Site/src/components/navbar.tsx
+++ Haircut_Site/src/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className = "sticky w-full h-24 bg-green-700 backdrop-blur-sm border-b border-green-900">
         <div className="flex flex-wrap justify-between items-center max-w-[1240px] text-white m-auto">
-            <h1 className='text-3xl font-bold font-serif mx-3 my-6'>SalonSense: Expert Recommendations at Your Fingertips</h1>
+            <h1 className='text-3xl font-bold font-serif mx-3 my-6'>SalonSense</h1>
             <ul className = "flex justify-between items-center ">
                 <li className='p-4 text-xl'><Link>Home</Link></li>
                 <li className='p-4 text-xl'><Link>Services</Link></li>
